161a9755595566fc57ed0b2315c11ff41cccaccd
Darren Haynes <darren.haynes.mr@outlook.com>
2025-07-06 23:27:26 -0500

    Trimmed excess print checks used for testing.

--- main.py
+++ main.py
@@ -11,14 +11,5 @@ def activity_count(camp, activity, num=1):
 
 if __name__ == "__main__":
     camp = SummerCamp()
-    print(camp.land_inbetween)
-    print(camp.land_below_min)
-    print(camp.calc_kids_min())
-    print(camp.calc_kids_max())
     camp.assign_land()
     camp.print_land(bounds=True)
-    print(camp.land_9am_count)
-    print(camp.land_10am_count)
-    print(camp.land_inbetween)
-    # print("WHAT ON EARTH")
-    # print(camp.filter_sort_land_inbetween())
