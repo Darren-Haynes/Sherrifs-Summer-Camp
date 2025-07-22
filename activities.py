d179ca0be13dca6687f40fe4e0fcaf0bef61d061
Darren Haynes <darren.haynes.mr@outlook.com>
2025-07-21 21:12:42 -0500

    Changed activities to abbrebiated form.
    This is to match the input data from the client.

--- activities.py
+++ activities.py
@@ -2,73 +2,74 @@ land = {
     # tuple[0] min of num of people per land activity
     # tuple[1] max of num of people per land activity
     # tuple[2] 0 = 9am only, 1 = 10am only, 2 = 9am & 10am activities
-    "basketball": [6, 12, 0],
-    "volleyball": [6, 12, 0],
-    "soccer": [6, 12, 0],
-    "archery": [8, 16, 2],
-    "arts": [4, 10, 2],
-    "hiking": [4, 6, 2],
-    "cheer": [6, 12, 0],
-    "pickleball": [4, 12, 1],
-    "lacrosse": [6, 12, 1],
-    "football": [6, 12, 1],
-    "yoga": [6, 10, 1],
-    "frisbee": [3, 8, 1],
+    "bball": [6, 12, 0],  # baseball
+    "vball": [6, 12, 0],  # volleyball
+    "soc": [6, 12, 0],  # soccer
+    "arch": [8, 16, 2],  # archery
+    "art": [4, 10, 2],  # art
+    "hike": [4, 6, 2],  # hiking
+    "cheer": [6, 12, 0],  # cheer
+    "pball": [4, 12, 1],  # pickleball
+    "lax": [6, 12, 1],  # lacrosse
+    "fball": [6, 12, 1],  # football
+    "yoga": [6, 10, 1],  # yoga
+    "fris": [3, 8, 1],  # frisbee
 }
 
 water = {
     # tuple[0] min of num of people per water activity
     # tuple[1] max of num of people per water activity
-    "swimming": (6, 16, 2),
-    "fishing": (4, 7, 2),
-    "canoe": (6, 10, 2),
-    "snorkle": (4, 8, 2),
-    "sailing": (6, 16, 2),
-    "paddleboard": (4, 8, 2),
-    "kayak": (6, 8, 2),
+    # tuple[2] 0 = 9am only, 1 = 10am only, 2 = 9am & 10am activities
+    "swim": (6, 16, 2),  # swimming
+    "fish": (4, 7, 2),  # fishing
+    "canoe": (6, 10, 2),  # canoe
+    "snork": (4, 8, 2),  # snorkle
+    "sail": (6, 16, 2),  # sailing
+    "pboard": (4, 8, 2),  # paddleboard
+    "kayak": (6, 8, 2),  # kayak
 }
 
 nineAM_land = {
     # Land activities offered for the 9am slot
     # Boolean represents if the time slot is full (True)
-    "basketball": [],
-    "volleyball": [],
-    "soccer": [],
-    "archery": [],
-    "arts": [],
-    "hiking": [],
+    "bball": [],
+    "vball": [],
+    "soc": [],
+    "arch": [],
+    "art": [],
+    "hike": [],
     "cheer": [],
 }
 
 nineAM_water = {
     # Water activities offered for the 9am slot
-    "swimming": [],
-    "fishing": [],
+    "swim": [],
+    "fish": [],
     "canoe": [],
     "kayak": [],
-    "snorkle": [],
-    "sailing": [],
-    "paddleboard": [],
+    "snork": [],
+    "sail": [],
+    "pboard": [],
 }
 tenAM_land = {
     # Land activities offered for the 10am slot
-    "pickleball": [],
-    "lacrosse": [],
-    "football": [],
-    "archery": [],
-    "arts": [],
-    "hiking": [],
+    "pball": [],
+    "lax": [],
+    "fball": [],
+    "arch": [],
+    "art": [],
+    "hike": [],
     "yoga": [],
-    "frisbee": [],
+    "fris": [],
 }
 
 tenAM_water = {
     # Water activities offered for the 10am slot
-    "swimming": [],
-    "fishing": [],
+    "swim": [],
+    "fish": [],
     "canoe": [],
     "kayak": [],
-    "snorkle": [],
-    "sailing": [],
-    "paddleboard": [],
+    "snork": [],
+    "sail": [],
+    "pboard": [],
 }
